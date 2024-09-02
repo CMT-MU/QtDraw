297b96920805998a8f7584a4705beab9520fd238
HK <hk@meiji.ac.jp>
2024-09-02 20:29:23 +0900

    version up

--- qtdraw/__init__.py
+++ qtdraw/__init__.py
@@ -4,7 +4,7 @@ QtDraw
 
 from pathlib import Path
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __date__ = "2021 - 2024"
 __author__ = "Hiroaki Kusunose"
 __top_dir__ = Path(__file__).parent / ".."
