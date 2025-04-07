98444178406e1e1d39675cc3b6d8a74fa7d31637
HK <hk@meiji.ac.jp>
2025-04-07 15:58:21 +0900

    fix xsf parser w.r.t. origin choice

--- qtdraw/parser/read_material.py
+++ qtdraw/parser/read_material.py
@@ -4,7 +4,9 @@ Simple parser for CIF, XSF, and VESTA.
 This module contains the parser.
 """
 
+import numpy as np
 from qtdraw.parser.util import parse_material, draw_site_bond
+from qtdraw.parser.xsf import extract_data_xsf
 
 
 # ==================================================
@@ -26,7 +28,13 @@ def read_draw(filename, widget):
     draw_site_bond(widget, name, site_info, bond_info)
 
     if filename.endswith(".xsf"):
-        pos = site_info[0][3]
-        widget.add_isosurface(data=filename, position=pos, color="Pastel1", surface="phase")
+        # determine color_range and value.
+        data = np.array(extract_data_xsf(filename)["data"])
+        d_max = float(data.max())
+        d_min = float(data.min())
+        d_max = 1.1 * d_max if d_max > 0.0 else 0.9 * d_max
+        d_min = 0.9 * d_min if d_min > 0.0 else 1.1 * d_min
+        val = 0.5 * (d_max + d_min)
+        widget.add_isosurface(data=filename, color="Pastel1", surface="phase", color_range=[d_min, d_max], value=[val])
 
     return all_data
