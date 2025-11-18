f982d2084e005472e87b8be424e07d3c800a19cf
HK <hk@meiji.ac.jp>
2025-11-18 16:35:50 +0900

    minor change

--- others/tests/test_pyvistaqt.py
+++ others/tests/test_pyvistaqt.py
@@ -6,8 +6,23 @@ This module provides a test for PyVistaQt BackgroundPlotter.
 
 import pyvista as pv
 from pyvistaqt import BackgroundPlotter
+from PySide6.QtWidgets import QApplication
 
-plotter = BackgroundPlotter(show=True)
-plotter.add_mesh(pv.Sphere())
 
-plotter.app.exec()
+class Plotter(BackgroundPlotter):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def paintEvent(self, event):
+        # override the function to do nothing for PySide 6.10 or later.
+        pass
+
+
+app = QApplication([])
+
+sphere = pv.Sphere()
+
+plotter = Plotter(app=app)
+plotter.add_mesh(sphere)
+
+app.exec()
