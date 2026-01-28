2f1503c3fdf474cd93ff9bbda53e024c0592d1db
HK <hk@meiji.ac.jp>
2026-01-28 14:20:53 +0900

    version up

--- qtdraw/widget/qt_event_util.py
+++ qtdraw/widget/qt_event_util.py
@@ -8,7 +8,7 @@ import sys
 import logging
 from IPython.core import ultratb
 from PySide6.QtCore import QObject, Signal, Qt
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QFont, QPalette, QColor
 from PySide6.QtWidgets import QApplication
 
 from qtdraw.core.pyvista_widget_setting import default_preference
@@ -59,6 +59,23 @@ def get_qt_application():
     size = default_preference["general"]["size"]
     app.setStyle(style)
     app.setFont(QFont(font, size))
+
+    # use light-mode palette.
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(240, 240, 240))
+    palette.setColor(QPalette.WindowText, Qt.black)
+    palette.setColor(QPalette.Base, Qt.white)
+    palette.setColor(QPalette.AlternateBase, QColor(233, 231, 227))
+    palette.setColor(QPalette.ToolTipBase, Qt.white)
+    palette.setColor(QPalette.ToolTipText, Qt.black)
+    palette.setColor(QPalette.Text, Qt.black)
+    palette.setColor(QPalette.Button, QColor(240, 240, 240))
+    palette.setColor(QPalette.ButtonText, Qt.black)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.white)
+    app.setPalette(palette)
 
     return app
 
